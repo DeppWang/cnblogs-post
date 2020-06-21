c643d9b3ce1166aec4667284357bffec0f08b8ef
DeppWang <deppwxq@gmail.com>
2020-06-21 15:54:27 +0800

    Optimize code

--- cnblogs_post.py
+++ cnblogs_post.py
@@ -4,17 +4,17 @@ import os
 import sys
 import logging
 
-# logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
 # 设置 ssl 很重要，否则将报 ssl 错
 ssl._create_default_https_context = ssl._create_unverified_context
 
 # dict
 config = {
-    'url': 'https://rpc.cnblogs.com/metaweblog/deppwang',
-    'username': 'DeppWangXQ',  # username 为登录用户名，可能跟上面的不一致
-    'password': '12345678',
-    'local_post_path': '/Users/yanjie/GitHub/HexoBlog/source/_posts/'
+    'url': 'https://rpc.cnblogs.com/metaweblog/deppwang',              # 你的 MetaWeblog 访问地址
+    'username': 'DeppWangXQ',                                          # 你的登录用户名，可能跟上面的不一致
+    'password': '12345678',                                            # 你的登录密码
+    'local_post_path': '/Users/yanjie/GitHub/HexoBlog/source/_posts/'  # 你的本地博文路径
 }
 
 
@@ -48,16 +48,16 @@ def set_article(article_path):
     from markdown_it.extensions.front_matter import front_matter_plugin
     from markdown_it.extensions.footnote import footnote_plugin
 
-    # logging.info(article_path)
-
     with open(article_path, 'rb') as f:
         content = f.read().decode('utf-8')
 
     reg = r'(---(.|[\n])*?---)'
     p = re.compile(reg)
     headers = p.findall(content)
+
+    logging.info('headers: %s', headers)
     if len(headers) < 1:
-        raise ValueError('文章不存在 header --- ** ---，请检查！')
+        raise ValueError('文章 %s 不存在 header 信息块「--- ** ---」，请检查！' % article_path)
 
     header_str = headers[0][0]
 
@@ -65,28 +65,28 @@ def set_article(article_path):
 
     title, tags_categories = '', []
     for line in lines:
-        line_ele = line.split(':')
+        # 不使用':', 再替换所有空格为 null，因为可能存在中英文空格
+        line_ele = line.split(': ')
         if line_ele[0] == 'title':
-            title = line_ele[1].replace(' ', '')
-            # logging.info(title)
+            title = line_ele[1].rstrip()
 
         elif line_ele[0] == 'tags':
-            tags = line_ele[1].replace(' ', '')
+            tags = line_ele[1].rstrip()
             tags = tags.replace('[', '')
             tags = tags.replace(']', '')
-            # logging.info(tags)
             tags_categories.append(tags)
 
         # 将分类也作为标签
         elif line_ele[0] == 'categories':
-            categories = line_ele[1].replace(' ', '')
+            categories = line_ele[1].rstrip()
             tags_categories.insert(0, categories)
 
     separator = ', '
     tags = separator.join(tags_categories)
-    # logging.info(tags)
+    logging.info("tags: %s", tags)
 
-    content = content.replace(header_str, '')
+    # 只替换第一个
+    content = content.replace(header_str, '', 1)
 
     md = (
         MarkdownIt()
@@ -96,18 +96,20 @@ def set_article(article_path):
             .enable('table')
     )
 
+    # markdown 转换为 HTML
     content = md.render(content)
+
     # cnblogs-markdown 属性用于代码块样式
     content = '<div class=\"cnblogs-markdown\">%s</div>' % content
-    # print(content)
+
     if title is '' or content is '' or tags is '':
-        raise ValueError('文章 title 或 content 或 tags 为空，请检查！')
+        raise ValueError('文章 title、content、tags 均不能为空；\': \'后有空格，请检查！')
 
     article = {'title': title, 'content': content, 'tags': tags}
     return article
 
 
-def find_last_modified_file(file_count):
+def get_local_modified_file(file_count):
     import stat
     import datetime as dt
 
@@ -119,69 +121,83 @@ def find_last_modified_file(file_count):
                 unix_modified_time = os.stat(os.path.join(root, file))[stat.ST_MTIME]
                 human_modified_time = dt.datetime.fromtimestamp(unix_modified_time).strftime('%Y%m%dT%H:%M:%S')
                 filename = os.path.join(root, file)
-                # logging.info(os.path.splitext(filename)[1])
                 if os.path.splitext(filename)[1] == '.md':
                     modified.append((human_modified_time, filename))
             except:
                 pass
 
     modified.sort(key=lambda a: a[0], reverse=True)
-    # logging.info(modified[0: file_count])
     return modified[0: file_count]
 
 
-def edit_or_new(metaWeblog, posts, filename):
-    try:
-        article = set_article(filename)
-    except Exception as err:
-        print(format(err))
-        sys.exit(-1)
+def delete():
+    """ 默认删除最新文章 """
+
+    metaWeblog = MetaWeblog(config['url'], config['username'], config['password'])
+    posts = metaWeblog.getRecentPosts(100)
+    postid = posts[0]['postid']
+    title = posts[0]['title']
+    status = metaWeblog.deletePost(postid)
+    if status is True:
+        print('删除「%s」成功！' % title)
+    else:
+        print('删除「%s」失败，文章不存在。文章 id 为：%s' % (title, postid))
+
+
+def post(count):
+
+    modified_files = get_local_modified_file(count)
+    for modified_file in modified_files:
+        edit_or_new(modified_file[1])
+
+
+def judge_str_equal(str1, str2):
+    str1 = str1.replace(' ', '')
+    str2 = str2.replace(' ', '')
+    return str1 == str2
+
+
+def edit_or_new(article_path):
+
+    blog = MetaWeblog(config['url'], config['username'], config['password'])
+    posts = blog.getRecentPosts(100)
+    article = set_article(article_path)
 
     # 如果存在，更新，否则新增
     exist_flag = 0
 
     for post in posts:
-        if post['title'] == article['title']:
-            status = metaWeblog.editPost(post['postid'], article)
+        # 判断是否含有相同的字符，不直接用等号
+        if judge_str_equal(post['title'], article['title']):
+            status = blog.editPost(post['postid'], article)
             if status is True:
-                # logging.info(article['title'])
                 print("更新文章「%s」成功" % article['title'])
-            # logging.info(status)
             exist_flag = 1
             break
 
     if exist_flag == 0:
-        id = metaWeblog.newPost(article)
+        id = blog.newPost(article)
         if id is not None:
             print("发布文章成功，文章 id 为 %s" % id)
 
 
 def main():
 
-    metaWeblog = MetaWeblog(config['url'], config['username'], config['password'])
+    try:
+        print('正在发布 ...')
+        if len(sys.argv) > 1 and sys.argv[1] == 'delete':
+            delete()
 
-    posts = metaWeblog.getRecentPosts(100)
+        elif len(sys.argv) > 1 and isinstance(int(sys.argv[1]), int):
+            logging.info("sys.argv[1]: %s", sys.argv[1])
+            post(int(sys.argv[1]))
 
-    if len(sys.argv) > 1 and sys.argv[1] == 'delete':
-        postid = posts[0]['postid']
-        title = posts[0]['title']
-        status = metaWeblog.deletePost(postid)
-        if status is True:
-            print('删除 %s 成功！' % title)
         else:
-            print('删除 %s 失败，不存在。文章 id 为：%s' % (title, postid))
+            post(1)
+    except Exception as err:
+        print(format(err))
+        print('发布失败')
         sys.exit(-1)
-
-    if len(sys.argv) > 1 and isinstance(int(sys.argv[1]), int):
-        logging.info("sys.argv[1]: %s", sys.argv[1])
-        modified_files = find_last_modified_file(int(sys.argv[1]))
-        for modified_file in modified_files:
-            edit_or_new(metaWeblog, posts, modified_file[1])
-
-        sys.exit(-1)
-
-    filename = find_last_modified_file(1)[0][1]
-    edit_or_new(metaWeblog, posts, filename)
 
 
 if __name__ == '__main__':
